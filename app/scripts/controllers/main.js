18f42b4f6f8c1b0de91ec3d5f765465ea3a1fac8
Andrew Byrne <abyrne@yahoo-inc.com>
2014-12-15 15:44:49 +0000

    showing moved jobs

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -15,37 +15,57 @@ angular.module('angulistApp')
       'Karma'
     ];
 
+$scope.showMovedJobs=false;
 
 
-
+var pool1location, pool2location;
+$scope.updatedPool1=[];
+$scope.updatedPool2=[];
 
  function initList(){
+
     $http.get('jobs.json').success(function(data){
+
   		$scope.jobs = data;
+
       $scope.pool1 = $scope.jobs[0];
       $scope.pool2 = $scope.jobs[1];
+
+      pool1location = $scope.pool1[0].pool;
+      pool2location = $scope.pool2[0].pool;
+
     });
   }
   initList();
 
 
-
-  
   $scope.sortableOptions = {
+
     placeholder: 'job',
     connectWith: '.jobs-container',
-    stop: function(e, ui) {
-    
-    }
   };
 
     
   $scope.resetList = function(){
+    $scope.showMovedJobs=false;
     initList();
   };
 
   $scope.updateList = function(){
-   
+
+
+    for(var i=0;i<$scope.pool1.length;i++){
+      if($scope.pool1[i].pool !== pool1location){
+        $scope.updatedPool1.push($scope.pool1[i]);
+      }
+    }
+    for(var i=0;i<$scope.pool2.length;i++){
+      if($scope.pool2[i].pool !== pool2location){
+        $scope.updatedPool2.push($scope.pool2[i]);
+      }
+    }
+
+    $scope.showMovedJobs=true;
   };
 
 
